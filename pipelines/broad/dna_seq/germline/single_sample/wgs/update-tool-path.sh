d22e5f1380c652196a9d8f3664bc963e750e9809
priyanka sebastian <sebastian.priyanka@gmail.com>
2021-11-16 13:52:28 -0800

    Update update-tool-path.sh
    updated tool paths with tool versions

--- pipelines/broad/dna_seq/germline/single_sample/wgs/update-tool-path.sh
+++ pipelines/broad/dna_seq/germline/single_sample/wgs/update-tool-path.sh
@@ -3,19 +3,20 @@
 ####### this bash script file updates the tool paths for onprem runs ############
 
 echo "changing picard tool path"
+#Picard 2.16.0 is used.
 sed -i  's|/usr/picard/picard.jar|/mnt/lustre/genomics/tools/picard.jar|g' *.wdl
 sed -i  's|/usr/gitc/picard-private.jar|/mnt/lustre/genomics/tools/picard.jar|g' *.wdl
 sed -i  's|/usr/gitc/picard.jar|/mnt/lustre/genomics/tools/picard.jar|g' *.wdl
 
 
 echo "updating tool path for gatk"
-sed -i 's|gatk --java|/mnt/lustre/genomics/tools/gatk/gatk --java|g' *.wdl
-sed -i 's|/root/gatk.jar|/mnt/lustre/genomics/tools/gatk.jar --java|g' *.wdl
+sed -i 's|gatk --java|/mnt/lustre/genomics/tools/gatk-4.1.9.0/gatk --java|g' *.wdl
+sed -i 's|/root/gatk.jar|/mnt/lustre/genomics/tools/gatk-4.1.9.0/gatk-package-4.1.9.0-local.jar --java|g' *.wdl
 
 
 echo "updating tool path for samtools"
-sed -i 's|samtools|/mnt/lustre/genomics/tools/samtools/samtools |g' *.wdl
-sed -i 's|seq_cache_populate\.pl|/mnt/lustre/genomics/tools/samtools/misc/seq_cache_populate\.pl |g' *.wdl
+sed -i 's|samtools|/mnt/lustre/genomics/tools/samtools-1.9/samtools |g' *.wdl
+sed -i 's|seq_cache_populate\.pl|/mnt/lustre/genomics/tools/samtools-1.9/misc/seq_cache_populate\.pl |g' *.wdl
 
 echo "updating tool path for verifybamid"
 sed -i 's|/usr/gitc/VerifyBamID|/mnt/lustre/genomics/tools/VerifyBamID/bin/VerifyBamID |g' BamProcessing.wdl
@@ -23,4 +24,4 @@ sed -i 's|/usr/gitc/VerifyBamID|/mnt/lustre/genomics/tools/VerifyBamID/bin/Verif
 
 echo "updating tool path for bwa"
 sed -i 's|/usr/gitc/bwa|/mnt/lustre/genomics/tools/bwa-0.7.17/bwa|g' *.wdl
-sed -i 's|/usr/gitc/~{bwa_commandline}|/mnt/lustre/genomics/tools/bwa/~{bwa_commandline}|g' *.wdl
+sed -i 's|/usr/gitc/~{bwa_commandline}|/mnt/lustre/genomics/tools/bwa-0.7.17/~{bwa_commandline}|g' *.wdl
